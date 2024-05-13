ede4d1635972a5f3b73d586c8d852fe4c6acd828
Farhad-Hossain <farhad.cse@yahoo.com>
2024-05-14 00:18:57 +0600

    Master Layout page

--- resources/views/inc/topbar.blade.php
+++ resources/views/inc/topbar.blade.php
@@ -1,31 +1,27 @@
-<!-- Topbar -->
-			<div class="topbar">
-				<div class="container">
-					<div class="row">
-						<div class="col-lg-6 col-md-5 col-12">
-							<!-- Contact -->
-							<ul class="top-link">
-								<li><a href="#">About</a></li>
-								<li><a href="{{ route('contact') }}">Contact</a></li>
-								{{--
-								<li><a href="#">FAQ</a></li>
-								--}}
-							</ul>
-							<!-- End Contact -->
-						</div>
-						<div class="col-lg-6 col-md-7 col-12">
-							<!-- Top Contact -->
-							<ul class="top-contact">
-								<li><i class="fa fa-phone"></i>
-                                   <a href="tel:{{$gs?$gs->hotline_no:''}}">{{$gs?$gs->hotline_no:''}}</a> 
-                                </li>
-								<li><i class="fa fa-envelope"></i>
-                                    <a href="mailto:{{ $gs ? $gs->contact_email : '' }}">{{ $gs ? $gs->contact_email : '' }}</a>
-                                </li>
-							</ul>
-							<!-- End Top Contact -->
-						</div>
-					</div>
-				</div>
+<div class="topbar">
+	<div class="container">
+		<div class="row">
+			<div class="col-lg-6 col-md-5 col-12">
+				<ul class="top-link">
+					<li><a href="#">About</a></li>
+					<li><a href="{{ route('contact') }}">Contact</a></li>
+					{{--
+					<li><a href="#">FAQ</a></li>
+					--}}
+				</ul>
 			</div>
-			<!-- End Topbar -->
+			<div class="col-lg-6 col-md-7 col-12">
+	
+				<ul class="top-contact">
+					<li><i class="fa fa-phone"></i>
+						<a href="tel:{{$gs?$gs->hotline_no:''}}">{{$gs?$gs->hotline_no:''}}</a> 
+					</li>
+					<li><i class="fa fa-envelope"></i>
+						<a href="mailto:{{ $gs ? $gs->contact_email : '' }}">{{ $gs ? $gs->contact_email : '' }}</a>
+					</li>
+				</ul>
+	
+			</div>
+		</div>
+	</div>
+</div>
