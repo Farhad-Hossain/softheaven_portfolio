41587231a5fbfece4d7545416dbc9060f1daea67
farhad.cse33 <farhad.cse33@gmail.com>
2026-01-01 23:47:56 +0600

    slider edit

--- app/Http/Controllers/Admin/SliderController.php
+++ app/Http/Controllers/Admin/SliderController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use App\Models\Slider;
 use Illuminate\Http\Request;
+use helpers;
 
 class SliderController extends Controller
 {
--- app/helpers.php
+++ app/helpers.php
@@ -16,22 +16,20 @@ if ( !function_exists('shortenText') ) {
     }   
 }
 
-if ( !function_exists('get_file_info') ) {
-    function get_file_info($file)
-    {
-        $file_info = [
-            'name' => '',
-            'size' => '',
-            'extension' => '',
-        ];
+function get_file_info($file)
+{
+    $file_info = [
+        'name' => '',
+        'size' => '',
+        'extension' => '',
+    ];
 
-        if ( !is_object($file) || !$file instanceof \Illuminate\Http\File ) {
-            return $file_info;
-        }
-
-        $file_info['name'] = $file->getClientOriginalName();
-        $file_info['size'] = $file->getSize();
-        $file_info['extension'] = $file->getClientOriginalExtension();
+    if ( !is_object($file) || !$file instanceof \Illuminate\Http\File ) {
         return $file_info;
     }
+
+    $file_info['name'] = $file->getClientOriginalName();
+    $file_info['size'] = $file->getSize();
+    $file_info['extension'] = $file->getClientOriginalExtension();
+    return $file_info;
 }
